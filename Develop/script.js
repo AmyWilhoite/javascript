d92abfa575889cfaba48e89791fe741e9cd73f65
AmyWilhoite <amywilhoite@gmail.com>
2022-01-15 23:57:42 -0500

    created outline inscript file for formulas

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("confimation it works");
+
+  //prompt user for password criteria
+  // a - between 8 and 128 defined as "input"
+  var input = prompt ("Please enter the number of characters you prefer (you may select between 8-120 characters).");
+  console.log(input);
+
+  // b - lower upper number special
+
+
+  //validate the input (above)
+  //user "input" must be 8-128 characters
+
+  
+  //generate password 
+
+  //display password on page
+  return "Your password is variable formula"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
